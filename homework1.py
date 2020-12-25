54a1042764a01613cd3cdbf2e72d8522a66b377c
Elif Tonbul <54376991+eliftnbl@users.noreply.github.com>
2020-12-25 20:07:40 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,16 @@
+
+
+a=int(input("Enter integer value : "))
+b=float(input("Enter float value : "))
+c=str(input("Enter string value : "))
+d=str(input("Enter string value : "))
+e=int(input("Enter integer value : "))
+
+print("First value : {}".format(a),"/", f'First value type : {type(a)}')
+print("Second value : {}".format(b),"/", f'Second value type : {type(b)}')
+print("Third value : {}".format(c),"/", f'Third value type: {type(c)}')
+print("Forth value : {}".format(d),"/", f'Forth value type : {type(d)}')
+print("Fifth value : {}".format(e),"/", f'Fifth value type : {type(e)}')
+
+
+
